458da4fdbb9bd9abfe2e77a849ffc5cbe7d7a023
Diya Arora <diya05arora@gmail.com>
2024-09-14 15:25:15 +0530

    changes for chatbot

--- chatbot/index.js
+++ chatbot/index.js
@@ -5,7 +5,7 @@ const cors = require('cors'); // Import the cors package
 const dotenv = require('dotenv');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors()); // Enable CORS
